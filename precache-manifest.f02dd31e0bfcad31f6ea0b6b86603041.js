7e56d4239a4ce22e75f8b2c283b348eb5049f546
vignesh <vigneshkrishnan4786@gmail.com>
2021-07-12 15:43:25 +0530

    Updates

---
+++ precache-manifest.f02dd31e0bfcad31f6ea0b6b86603041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64c44b8a7d7d5b0046b65ab762cc9b7",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "377cb60d8c4acd975bb5",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "9a2c874b6a175a0122b7",
+    "url": "/box-office/static/js/2.e4522165.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.e4522165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377cb60d8c4acd975bb5",
+    "url": "/box-office/static/js/main.43fae741.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
